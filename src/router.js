0285b1435dd1842a279e5adf32bf76c0ffef6b49
jsmask <2674800533@qq.com>
2019-10-31 15:22:48 +0800

    update

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import Medium from './views/home/Medium.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/taptap/'
+    : '/',
     outputDir: 'dist',
     productionSourceMap: true,
     assetsDir: "static",
